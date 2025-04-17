9d99f41ffc2f644c6f4c5593a8f79ee38b4c833f
Claudio <claudiosouzadias2013@gmail.com>
2025-04-16 22:17:00 -0300

    Project Base

---
+++ src/main/java/com/example/panda_mangas/PandaMangasApplication.java
@@ -0,0 +1,13 @@
+package com.example.panda_mangas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaMangasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaMangasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/panda_mangas/PandaMangasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.panda_mangas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaMangasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
